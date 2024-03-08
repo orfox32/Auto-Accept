7f0c264f511acccc13e66f64c8fba1292fbce678
Orfox <77915094+orfox32@users.noreply.github.com>
2024-03-08 15:38:33 +0800

    Delete include directory

--- include/config.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include <string>
-
-void saveUserDirectory(const std::string& directory);
-std::string readUserDirectory();
-std::string getUserDirectory();
--- include/utils.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include <string>
-
-std::string findLockFile(const std::string& gamedir);
-bool isLockFileExists(const std::string& lockfilePath);
