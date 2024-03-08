d7230202794e161af84eb85fd86f21f7a5c4aa65
Orfox <77915094+orfox32@users.noreply.github.com>
2024-03-08 17:10:30 +0800

    Get summoner id

--- include/http_request.h
+++ include/http_request.h
@@ -5,5 +5,6 @@
 
 bool sendHttpRequest(const std::string &url, const std::string &authorization, std::string &response, const std::string &requestType);
 bool checkLoggedIn(const std::string &port, const std::string &authorization);
+bool getSummonerData(const std::string& port, const std::string& authorization, std::string& summonerData);
 
 #endif // HTTP_REQUEST_H
