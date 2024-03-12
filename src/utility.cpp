712c9de04e5ec68afe80f713b258395f63c39399
Orfox <77915094+orfox32@users.noreply.github.com>
2024-03-13 01:30:25 +0800

    reverting to old, too many bugs

--- src/utility.cpp
+++ src/utility.cpp
@@ -9,8 +9,8 @@
 #include <thread>
 #include <nlohmann/json.hpp>
 #include <iomanip>
-#include <fstream>
 #include <windows.h>
+#include <conio.h>
 
 using namespace std;
 using json = nlohmann::json;
@@ -60,171 +60,80 @@ int getConsoleWidth() {
     return csbi.srWindow.Right - csbi.srWindow.Left + 1;
 }
 
-void centerText(const string& text) {
+void centerText(const string &text){
     int consoleWidth = getConsoleWidth();
     int textLength = text.length();
     int leftPadding = (consoleWidth - textLength) / 2;
-    cout << setw(leftPadding + textLength) << right << text << "\r" << std::flush;
+    cout << setw(leftPadding + textLength) << right << text << "\r" << std::flush;  
 }
 
-void toggleAutoAccept(bool &autoAcceptOn, json &config)
+bool handleGamePhases(const string &port, const string &authorization, int retryDelaySeconds, bool loggedIn)
 {
-    HANDLE hConsoleInput;
-    INPUT_RECORD irInput;
-    DWORD dwEvents;
-    INPUT_RECORD irKey;
 
-    hConsoleInput = GetStdHandle(STD_INPUT_HANDLE);
-
-    HANDLE hConsoleOutput;
-    DWORD dwWritten;
-    COORD position; 
-
-    hConsoleOutput = GetStdHandle(STD_OUTPUT_HANDLE);
-
-    position.X = 1;
-    position.Y = 13; 
-
-    string statusMessage;
-    if (autoAcceptOn) {
-        statusMessage = "On";
-    } else {
-        statusMessage = "Off";
-    }
-
-    string message = "Auto Accept: " + statusMessage + " (Press Up arrow key to On and Down arrow key to Off. Then press Enter Key. R to edit choice)";
-
-    do
+    while (loggedIn)
     {
-        SetConsoleCursorPosition(hConsoleOutput, position);
-
-        int consoleWidth = getConsoleWidth();
-
-        int messageWidth = message.length();
-
-        int leftPadding = (consoleWidth - messageWidth) / 2;
-
-        cout << setw(leftPadding + messageWidth) << right << message << "\r" << std::flush;
-
-
-        ReadConsoleInput(hConsoleInput, &irKey, 1, &dwEvents);
-        if (irKey.EventType == KEY_EVENT && irKey.Event.KeyEvent.bKeyDown)
+        if (!LeagueClientIsOpen())
         {
-            if (irKey.Event.KeyEvent.wVirtualKeyCode == VK_UP)
-            {
-                autoAcceptOn = true;
-            }
-            else if (irKey.Event.KeyEvent.wVirtualKeyCode == VK_DOWN)
-            {
-                autoAcceptOn = false;
-            }
-
-            string newStatusMessage = autoAcceptOn ? "On" : "Off";
-            message = "Auto Accept: " + newStatusMessage + " (Press Up arrow key to On and Down arrow key to Off. Then press Enter Key. R to edit choice)";
-    
-        }
-    } while (irKey.Event.KeyEvent.wVirtualKeyCode != VK_RETURN);
-
-    config["autoAccept"] = autoAcceptOn;
-
-    ofstream configFile("config.json");
-    configFile << setw(4) << config << endl;
-}
-
-
-json loadConfig() {
-    json config;
-    ifstream configFile("config.json");
-    if (configFile.is_open()) {
-        configFile >> config;
-        configFile.close();
-    } else {
-        config["autoAccept"] = true;
-        ofstream newConfigFile("config.json");
-        newConfigFile << setw(4) << config << endl;
-        newConfigFile.close();
-        cout << "Created new config file with default settings." << endl;
-    }
-    return config;
-}
-
-
-bool handleGamePhases(const string& port, const string& authorization, bool autoAcceptOn, int retryDelaySeconds, bool loggedIn, json &config) {
-
-    HANDLE hConsoleInput;
-    INPUT_RECORD irKey;
-    DWORD dwEvents;
-
-    hConsoleInput = GetStdHandle(STD_INPUT_HANDLE);
-
-    HANDLE hConsoleOutput;
-    DWORD dwWritten;
-    COORD position; 
-
-    hConsoleOutput = GetStdHandle(STD_OUTPUT_HANDLE);
-
-    position.X = 0;
-    position.Y = 14; 
-
-    while (loggedIn) {
-        if (!LeagueClientIsOpen()) {
             cerr << "League client has been closed. Exiting..." << endl;
             return false;
         }
 
-        ReadConsoleInput(hConsoleInput, &irKey, 1, &dwEvents);
-        if (irKey.EventType == KEY_EVENT && irKey.Event.KeyEvent.bKeyDown && irKey.Event.KeyEvent.wVirtualKeyCode == 'R') {
-            toggleAutoAccept(autoAcceptOn, config);
-        }
-
-
         string phaseUrl = "https://127.0.0.1:" + port + "/lol-gameflow/v1/gameflow-phase";
         string phaseResponse;
-        if (sendHttpRequest(phaseUrl, authorization, phaseResponse, "GET", retryDelaySeconds)) {
-            try {
+        if (sendHttpRequest(phaseUrl, authorization, phaseResponse, "GET", retryDelaySeconds))
+        {
+            try
+            {
                 json phaseJson = json::parse(phaseResponse);
                 string phase = phaseJson.get<string>();
 
-                if (phase == "ChampSelect") {
-                    cout << setw(100) << left << " " << "\r" << std::flush;
+                if (phase == "ChampSelect")
+                {
                     cout << "ChampSelect phase detected. Waiting..." << "\r" << std::flush;
-                    while (phase == "ChampSelect") {
+                    while (phase == "ChampSelect")
+                    {
                         std::this_thread::sleep_for(std::chrono::seconds(1));
-                        if (!LeagueClientIsOpen()) {
+                        if (!LeagueClientIsOpen())
+                        {
                             cerr << "League client has been closed. Exiting..." << endl;
                             return false;
                         }
                         string phaseResponse;
-                        if (sendHttpRequest(phaseUrl, authorization, phaseResponse, "GET", retryDelaySeconds)) {
+                        if (sendHttpRequest(phaseUrl, authorization, phaseResponse, "GET", retryDelaySeconds))
+                        {
                             json phaseJson = json::parse(phaseResponse);
                             phase = phaseJson.get<string>();
-                        } else {
+                        }
+                        else
+                        {
                             cerr << "Failed to retrieve gameflow phase." << endl;
                             break;
                         }
                     }
-                } else {
-                    SetConsoleCursorPosition(hConsoleOutput, position);
+                }
+                else
+                {
                     cout << setw(100) << left << " " << "\r" << std::flush;
                     cout << "Current phase: " << phase << "\r" << std::flush;
                 }
 
-                if (phase == "ReadyCheck") {
-                    if (autoAcceptOn) {
-                        string acceptUrl = "https://127.0.0.1:" + port + "/lol-matchmaking/v1/ready-check/accept";
-                        string acceptResponse;
-                        if (!sendHttpRequest(acceptUrl, authorization, acceptResponse, "POST", retryDelaySeconds)) {
-                            cerr << "Failed to auto-accept match." << endl;
-                        }
-                    } else {
-                        cout << setw(100) << left << " " << "\r" << std::flush;
-                        cout << "Auto accept is turned off. Skipping ready-check." << "\r" << std::flush;
+                if (phase == "ReadyCheck")
+                {
+                    string acceptUrl = "https://127.0.0.1:" + port + "/lol-matchmaking/v1/ready-check/accept";
+                    string acceptResponse;
+                    if (!sendHttpRequest(acceptUrl, authorization, acceptResponse, "POST", retryDelaySeconds))
+                    {
+                        cerr << "Failed to auto-accept match." << endl;
                     }
-                } 
-            } catch (json::exception& e) {
+                }
+            }
+            catch (json::exception &e)
+            {
                 cerr << "Error parsing JSON response: " << e.what() << endl;
             }
-        } else {
+        }
+        else
+        {
             cerr << "Failed to retrieve gameflow phase." << endl;
         }
 
@@ -240,6 +149,10 @@ bool authenticateAndAutoAccept(const string &port, const string &authorization, 
 
     while (!loggedIn)
     {
+        cout << setw(100) << left << " " << "\r" << std::flush;
+        centerText("Checking login status...");
+        std::this_thread::sleep_for(std::chrono::seconds(5));
+
         try
         {
             if (!checkLoggedIn(port, authorization, retryDelaySeconds))
@@ -248,21 +161,22 @@ bool authenticateAndAutoAccept(const string &port, const string &authorization, 
                 std::this_thread::sleep_for(std::chrono::seconds(1));
                 continue;
             }
-
-            loggedIn = true;
             
-            centerText("Login successful!"); 
+            loggedIn = true;
+            cout << setw(100) << left << " " << "\r" << std::flush;
+            centerText("Login successful!");
             std::this_thread::sleep_for(std::chrono::seconds(5));
-
             
             string summonerData;
             if (getSummonerData(port, authorization, summonerData, retryDelaySeconds))
-            {   
+            {
                 try
                 {
+
                     auto summonerJson = json::parse(summonerData);
-                    string SummonerId = to_string(summonerJson["summonerId"]);
-                    centerText("Summoner ID loaded: " + SummonerId);
+                    string gameName = std::regex_replace(summonerJson["gameName"].dump(), std::regex("\""), "");
+                    string tagLine = std::regex_replace(summonerJson["tagLine"].dump(), std::regex("\""), "");
+                    centerText("Log in as: [" + gameName + ":" + tagLine + "]");
                     cout << "\n";
                 }
                 catch (const json::exception &e)
@@ -274,12 +188,8 @@ bool authenticateAndAutoAccept(const string &port, const string &authorization, 
             {
                 cerr << "Failed to retrieve summoner data." << endl;
             }
-            
-            json config = loadConfig();
-            bool autoAcceptOn = config.value("autoAccept", true);
-            toggleAutoAccept(autoAcceptOn, config);
 
-            handleGamePhases(port, authorization, retryDelaySeconds, autoAcceptOn, loggedIn, config);
+            handleGamePhases(port, authorization, retryDelaySeconds, loggedIn);
         }
         catch (const std::exception &e)
         {
@@ -287,5 +197,5 @@ bool authenticateAndAutoAccept(const string &port, const string &authorization, 
             return false;
         }
     }
-    return true;
+    return false;
 }
